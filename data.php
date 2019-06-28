cf96d76bfd3699f558859fe671c6f98a196e4ae1
xxxdoritosxxx <georgyvav@ya.ru>
2019-06-28 16:55:08 +0400

    check database

--- data.php
+++ data.php
@@ -1,2 +1,23 @@
 <?php
-echo "somedata";
+const BR = "<BR>";
+$db_info =[
+    "username"=>"Qhve9YdhKG",
+    "db_name"=>"Qhve9YdhKG",
+    "password"=>"AvVYDL39iH",
+    "server"=>"remotemysql.com",
+    "port"=>"3306"
+];
+
+$connecton = new mysqli(
+    $db_info['server'],
+    $db_info['username'],
+    $db_info['password'],
+    $db_info['db_name'],
+    $db_info['port']
+);
+
+$response = $connecton->query("SELECT * FROM articles");
+
+while( $row = $response->fetch_array() ){
+    echo implode(" ", $row) . "<br>";
+} 
