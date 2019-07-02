c6ceb71c831f89901c12f05281f75747203a7256
xxxdoritosxxx <georgyvav@ya.ru>
2019-07-02 08:59:53 +0400

    Update index.php

--- admin/panel/index.php
+++ admin/panel/index.php
@@ -10,6 +10,7 @@ if(!$_SESSION['admin']){
     <head>
         <title>admin panel</title>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <script src="/js/jquery.js"></script>
         <script src="/js/vue.js"></script>
