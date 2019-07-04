c26466de9f15056fe72e194852129c06f0a32329
xxxdoritosxxx <georgyvav@ya.ru>
2019-07-04 14:45:16 +0400

    fix menu visibility with new parallax

--- template/js/header.js
+++ template/js/header.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector("header");
     let currentOffsetY = 0;
-    document.addEventListener("scroll", function(event) {
-        if (event.pageY > currentOffsetY) { //scroll down
+
+    const main = document.querySelector("main");
+    main.addEventListener("scroll", function() {
+        if (main.scrollTop > currentOffsetY) { //scroll down
             if (header.classList.contains("show")) {
                 header.classList.remove("show");
             }
-        } else if (event.pageY < currentOffsetY) { //scroll up
+        } else if (main.scrollTop < currentOffsetY) { //scroll up
             if (!header.classList.contains("show")) {
                 header.classList.add("show");
             }
         }
 
-        currentOffsetY = event.pageY;
+        currentOffsetY = main.scrollTop;
     });
 });
