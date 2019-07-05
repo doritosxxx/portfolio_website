9e0230d0cdef47b5890113725a8c3c1b57787187
xxxdoritosxxx <georgyvav@ya.ru>
2019-07-05 18:11:25 +0400

    up button fix

--- js/up_button.js
+++ js/up_button.js
@@ -1,26 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    const main = $("main");
+
     $("#up").on("click", function() {
-        $("main").animate({
+        main.animate({
             scrollTop: 0,
         }, 1000);
     });
 
-    let previousScrollTop = 0;
+    let show = undefined;
 
-    $("#up").hide();
+    if (main.scrollTop() === 0) {
+        $("#up").hide();
+        show = false;
+    } else {
+        $("#up").show().css({
+            opacity: 1
+        });
+        show = true;
+    }
 
-    $("main").on("scroll", function(event) {
-        if (this.scrollTop > 0 && previousScrollTop === 0) {
+
+    main.on("scroll", function(event) {
+        if (!show && main.scrollTop() > 0) {
             $("#up").show().animate({
                 opacity: 1
             }, 400);
-        } else if (this.scrollTop === 0 && previousScrollTop > 0) {
+            show = true;
+        } else if (show && main.scrollTop() === 0) {
             $("#up").animate({
                 opacity: 0
             }, 400, function() {
                 $(this).hide();
+                show = false;
             });
         }
-        previousScrollTop = this.scrollTop;
     });
 });
