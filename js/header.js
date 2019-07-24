ee3d4a9e9875eacf43ac9a39bec8364c0a37cc98
xxxdoritosxxx <georgyvav@ya.ru>
2019-07-24 13:29:30 +0400

    hide menu padding when header is hidden

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector("header");
+    const menu = document.querySelector("#main_menu");
     let currentOffsetY = 0;
 
     const main = document.querySelector("main");
@@ -7,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
         if (main.scrollTop > currentOffsetY) { //scroll down
             if (header.classList.contains("show")) {
                 header.classList.remove("show");
+                menu.classList.remove("add_padding");
             }
         } else if (main.scrollTop < currentOffsetY) { //scroll up
             if (!header.classList.contains("show")) {
                 header.classList.add("show");
+                menu.classList.add("add_padding");
             }
         }
 
